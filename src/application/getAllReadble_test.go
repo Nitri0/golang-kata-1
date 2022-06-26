0a133ad003a8178965335e7d47e1d5cfeabbded0
Homero <homero.hernandez@walmart.com>
2022-06-26 12:52:32 -0400

    TDD: Red - useCase get All books and magazines list, should'nt be an empty list

--- src/application/getAllReadble_test.go
+++ src/application/getAllReadble_test.go
@@ -1,6 +1,15 @@
 package application
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestGetAllReadble_Invoke(t *testing.T) {
+
+	useCase := GetAllReadble{}
+	result, err := useCase.Invoke()
+
+	assert.NoError(t, err)
+	assert.NotZero(t, len(result))
 }
