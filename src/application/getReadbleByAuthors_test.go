db74af59745430a72e1e3a9775e9be463e44ab9b
Homero <homero.hernandez@walmart.com>
2022-06-26 16:34:40 -0400

    TDD: Red - usecase get Readble by authors with a valid email

--- src/application/getReadbleByAuthors_test.go
+++ src/application/getReadbleByAuthors_test.go
@@ -1,6 +1,33 @@
 package application
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestGetReadbleByAuthors_Invoke(t *testing.T) {
+	testTable := []struct {
+		testName        string
+		paramAuthor     []string
+		expectLenResult int
+		expectError     error
+	}{
+		{
+			"pass a valid email, should be return two result (a book and a magazine)",
+			[]string{"null-gustafsson@echocat.org"},
+			2,
+			nil,
+		},
+	}
+
+	useCase := GetReadbleByAuthors{}
+
+	for _, test := range testTable {
+		t.Run(test.testName, func(t *testing.T) {
+			result, err := useCase.Invoke(test.paramAuthor...)
+			assert.Equal(t, test.expectError, err)
+			assert.Equal(t, test.expectLenResult, len(result))
+		})
+
+	}
 }
